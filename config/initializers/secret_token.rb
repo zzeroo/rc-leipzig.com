b46dcdc04e6c9396ccdf78fbf40fb5a1ef97afa0
zzeroo <co@zzeroo.com>
2012-07-09 14:15:27 +0200

    Inital Commit, vanilla rails Rails 3.2.6

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RcLeipzigCom::Application.config.secret_token = '8e76e485c9cfc6e9572bf38d13b4a9acd76581653693fda141d62e3f692b8ecd9277b33928579f680144379708318fe8ddc5957a82fa441da4e37f38be178652'

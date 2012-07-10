48d3961ec41180ecd82e3ef68971dcf7b37c65ce
zzeroo <co@zzeroo.com>
2012-07-10 16:29:09 +0200

    Working nested resource version

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -2,53 +2,53 @@ class SubscribersController < ApplicationController
   # GET /subscribers
   # GET /subscribers.json
   def index
-    @subscribers = Subscriber.all
+    @event = Event.find(params[:event_id])
+    @subscribers = @event.subscribers.all
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @subscribers }
     end
   end
 
   # GET /subscribers/1
   # GET /subscribers/1.json
   def show
-    @subscriber = Subscriber.find(params[:id])
+    @event = Event.find(params[:event_id])
+    @subscriber = @event.subscribers.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @subscriber }
     end
   end
 
   # GET /subscribers/new
   # GET /subscribers/new.json
   def new
-    @subscriber = Subscriber.new
+    @event = Event.find(params[:event_id])
+    @subscriber = @event.subscribers.build
 
     respond_to do |format|
       format.html # new.html.erb
-      format.json { render json: @subscriber }
     end
   end
 
   # GET /subscribers/1/edit
   def edit
-    @subscriber = Subscriber.find(params[:id])
+    @event = Event.find(params[:event_id])
+    @subscriber = @event.subscribers.find(params[:id])
   end
 
   # POST /subscribers
   # POST /subscribers.json
   def create
-    @subscriber = Subscriber.new(params[:subscriber])
+    @event = Event.find(params[:event_id])
+    @subscriber = @event.subscribers.new(params[:subscriber])
 
     respond_to do |format|
       if @subscriber.save
-        format.html { redirect_to @subscriber, notice: 'Subscriber was successfully created.' }
-        format.json { render json: @subscriber, status: :created, location: @subscriber }
+        format.html { redirect_to @event, notice: 'Subscriber was successfully created.' }
       else
         format.html { render action: "new" }
-        format.json { render json: @subscriber.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -56,15 +56,14 @@ class SubscribersController < ApplicationController
   # PUT /subscribers/1
   # PUT /subscribers/1.json
   def update
-    @subscriber = Subscriber.find(params[:id])
+    @event = Event.find(params[:event_id])
+    @subscriber = @event.subscribers.find(params[:id])
 
     respond_to do |format|
       if @subscriber.update_attributes(params[:subscriber])
-        format.html { redirect_to @subscriber, notice: 'Subscriber was successfully updated.' }
-        format.json { head :no_content }
+        format.html { redirect_to @event, notice: 'Subscriber was successfully updated.' }
       else
         format.html { render action: "edit" }
-        format.json { render json: @subscriber.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -72,12 +71,12 @@ class SubscribersController < ApplicationController
   # DELETE /subscribers/1
   # DELETE /subscribers/1.json
   def destroy
-    @subscriber = Subscriber.find(params[:id])
+    @event = Event.find(params[:event_id])
+    @subscriber = @event.subscribers.find(params[:id])
     @subscriber.destroy
 
     respond_to do |format|
-      format.html { redirect_to subscribers_url }
-      format.json { head :no_content }
+      format.html { redirect_to events_url }
     end
   end
 end

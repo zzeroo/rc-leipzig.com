0c6f1843cc86781fb3f19fa55865dd470727b8ac
zzeroo <co@zzeroo.com>
2012-07-17 11:03:07 +0200

    Benutzername zur Benutzer Datenbank hinzugefügt
    Login ist dann auch mit dem Benutzernamen oder der Emailadresse möglich.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120717055244) do
+ActiveRecord::Schema.define(:version => 20120717090200) do
 
   create_table "events", :force => true do |t|
     t.date     "startdate"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(:version => 20120717055244) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "username"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true

d04d874c3dad14536a43a474f505d4457c9976f5
zzeroo <co@zzeroo.com>
2012-08-13 13:26:52 +0200

    Reorganisation der CSS DAteien

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 RcLeipzigCom::Application.initialize!
+
+config.action_mailer.delivery_method = :smtp
+
+ActionMailer::Base.smtp_settings = {
+  :address  => "mail.zzeroo.org",
+  :port  => 25,
+  :user_name  => "zzeroo",
+  :password  => "asrael",
+  :authentication  => :login
+}

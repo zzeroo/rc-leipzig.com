7d9693e2fd3960e6255d0d3d51b76476b90f258f
zzeroo <co@zzeroo.com>
2012-08-13 13:36:07 +0200

    Reconfig the Mailer settings

--- config/environment.rb
+++ config/environment.rb
@@ -4,8 +4,6 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 RcLeipzigCom::Application.initialize!
 
-config.action_mailer.delivery_method = :smtp
-
 ActionMailer::Base.smtp_settings = {
   :address  => "mail.zzeroo.org",
   :port  => 25,
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,10 @@ RcLeipzigCom::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
 
+  config.action_mailer.delivery_method = :smtp
+
+
+
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.raise_delivery_errors = true

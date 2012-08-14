08984fc6da76b3536f1137f11fe5fd82cf7a0966
zzeroo <co@zzeroo.com>
2012-08-14 17:43:53 +0200

    Uhrzeit in den Terminen entfernt
    Ich hatte erst geplant die Daten mit einer Uhrzeit (z.B. 12:00 Uhr) mit in der Datenbank zu speichern.
    
    Das ist aber sicher nicht sehr praktikabel, da man ja mit dem Kalender nur ein Datum auswählen kann. Die Uhrzeit sollte dann im Text zu den Terminen erscheinen.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,17 +8,17 @@
 
 puts "Creating events ..."
 events = Event.create([
-  { :startdate => "2012-01-28 12:00:00", :enddate => "2012-01-29 12:00:00",:title => "Sachsen GP & LE Moosi Cup", :location => "Racing Center Leipzig" },
-  { :startdate => "2012-02-04 12:00:00", :enddate => "2012-02-05 12:00:00",:title => "HPI-LRP Challenge", :location => "Racing Center Leipzig" },
-  { :startdate => "2012-02-11 12:00:00", :enddate => "2012-02-12 12:00:00",:title => "Tamiya Euro Cup", :location => "Racing Center Leipzig" },
-  { :startdate => "2012-03-31 12:00:00", :enddate => "2012-04-01 12:00:00",:title => "Tamiya Euro Cup", :location => "Racing Center Leipzig" },
-  { :startdate => "2012-04-14 12:00:00", :enddate => "2012-04-15 12:00:00",:title => "LRP Offroad Challenge", :location => "Racing Center Leipzig" },
-  { :startdate => "2012-05-19 12:00:00", :enddate => "2012-05-20 12:00:00",:title => "Sachsen GP & LE Moosi Cup", :location => "Racing Center Leipzig" },
-  { :startdate => "2012-06-23 12:00:00", :enddate => "2012-06-24 12:00:00",:title => "Sachsen GP & LE Moosi Cup", :location => "Racing Center Leipzig" },
-  { :startdate => "2012-09-01 12:00:00", :enddate => "2012-09-02 12:00:00",:title => "Sachsen GP & LE Moosi Cup", :location => "Racing Center Leipzig" },
-  { :startdate => "2012-10-05 12:00:00", :enddate => "2012-10-07 12:00:00",:title => "Hobby Modell Spiel", :location => "Racing Center Leipzig" },
-  { :startdate => "2012-11-17 12:00:00", :enddate => "2012-11-18 12:00:00",:title => "LRP Offroad Challenge", :location => "Racing Center Leipzig" },
-  { :startdate => "2012-12-01 12:00:00", :enddate => "2012-12-02 12:00:00",:title => "Tamiya Euro Cup", :location => "Racing Center Leipzig" }
+  { :startdate => "2012-01-28", :enddate => "2012-01-29",:title => "Sachsen GP & LE Moosi Cup", :location => "Racing Center Leipzig" },
+  { :startdate => "2012-02-04", :enddate => "2012-02-05",:title => "HPI-LRP Challenge", :location => "Racing Center Leipzig" },
+  { :startdate => "2012-02-11", :enddate => "2012-02-12",:title => "Tamiya Euro Cup", :location => "Racing Center Leipzig" },
+  { :startdate => "2012-03-31", :enddate => "2012-04-01",:title => "Tamiya Euro Cup", :location => "Racing Center Leipzig" },
+  { :startdate => "2012-04-14", :enddate => "2012-04-15",:title => "LRP Offroad Challenge", :location => "Racing Center Leipzig" },
+  { :startdate => "2012-05-19", :enddate => "2012-05-20",:title => "Sachsen GP & LE Moosi Cup", :location => "Racing Center Leipzig" },
+  { :startdate => "2012-06-23", :enddate => "2012-06-24",:title => "Sachsen GP & LE Moosi Cup", :location => "Racing Center Leipzig" },
+  { :startdate => "2012-09-01", :enddate => "2012-09-02",:title => "Sachsen GP & LE Moosi Cup", :location => "Racing Center Leipzig" },
+  { :startdate => "2012-10-05", :enddate => "2012-10-07",:title => "Hobby Modell Spiel", :location => "Racing Center Leipzig" },
+  { :startdate => "2012-11-17", :enddate => "2012-11-18",:title => "LRP Offroad Challenge", :location => "Racing Center Leipzig" },
+  { :startdate => "2012-12-01", :enddate => "2012-12-02",:title => "Tamiya Euro Cup", :location => "Racing Center Leipzig" }
 ])
 
 
@@ -28,10 +28,14 @@ users = User.create([
   { :username => "Tom", :email => "tomgehler@gmx.de", :password => "rc-leipzig", :password_confirmation => "rc-leipzig" },
   { :username => "Katrin", :email => "info@bremme-wohnen.de", :password => "rc-leipzig", :password_confirmation => "rc-leipzig" },
   { :username => "Thomas", :email => "thomesd@web.de", :password => "rc-leipzig", :password_confirmation => "rc-leipzig" },
-  { :username => "presse@rc-leipzig.com", :email  => "presse@rc-leipzig.com", :password => "rc-leipzig", :password_confirmation => "rc-leipzig" },
-  { :username => "info@rc-leipzig.com", :email  => "info@rc-leipzig.com", :password => "rc-leipzig", :password_confirmation => "rc-leipzig" },
+  { :username => "Presse", :email  => "presse@rc-leipzig.com", :password => "rc-leipzig", :password_confirmation => "rc-leipzig" },
+  { :username => "Info", :email  => "info@rc-leipzig.com", :password => "rc-leipzig", :password_confirmation => "rc-leipzig" },
   { :username => "Oliver", :email => "oliver.markgraf@googlemail.com", :password => "rc-leipzig", :password_confirmation => "rc-leipzig" },
 ])
 
-
+puts "Make the admins ..."
 User.find_by_username("zzeroo").update_attribute(:admin, true)
+User.find_by_username("Tom").update_attribute(:admin, true)
+User.find_by_username("Katrin").update_attribute(:admin, true)
+User.find_by_username("Thomas").update_attribute(:admin, true)
+User.find_by_username("Oliver").update_attribute(:admin, true)

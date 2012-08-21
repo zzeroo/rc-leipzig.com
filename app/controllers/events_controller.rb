6e35651d0f91dd58f762c72b4ba9c0986ef09593
zzeroo <co@zzeroo.com>
2012-08-21 21:35:44 +0200

    Sortierreihenfolge der Termine verändert.
    Die neuesten Termine stehen nun oben in der Übersicht.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -93,7 +93,7 @@ class EventsController < ApplicationController
   end
 
   def sort_direction
-    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "desc"
   end
 
 end

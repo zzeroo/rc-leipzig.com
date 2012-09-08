24258a055185e6068926aeaf215f696bc17aeeb1
zzeroo <co@zzeroo.com>
2012-09-08 09:34:49 +0200

    Benutzerübersicht verbessert
    Look and Feel der Terminübersicht angeglichen.
    - sortierbare Spalten
    - Aussehen gleich

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,6 +4,10 @@ class Admin::UsersController < ApplicationController
 
   helper_method :sort_column, :sort_direction
 
+  def index
+    @users = User.order(sort_column + ' ' + sort_direction)
+  end
+
   def new
   end
   
@@ -19,6 +23,16 @@ class Admin::UsersController < ApplicationController
       @current_user = nil
       # ...
     end
+  end
+
+  private
+  
+  def sort_column
+    User.column_names.include?(params[:sort]) ? params[:sort] : "last_sign_in_at"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
 
   def update_password
